63d36841fc0f00a6ff1c68b95c0cbded2b483279
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-02-21 21:34:38 -0300

    feat: chakra theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,9 +1,24 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-export const theme = extendTheme({
-  colors: {
-    brand: {
-      100: "#3d84f7",
+const config: ThemeConfig = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+export const theme = extendTheme(
+  { config },
+  {
+    colors: {
+      brand: {
+        100: "#3d84f7",
+      },
     },
-  },
-});
+    styles: {
+      global: () => ({
+        body: {
+          bg: "whiteAlpha.200",
+        },
+      }),
+    },
+  }
+);
