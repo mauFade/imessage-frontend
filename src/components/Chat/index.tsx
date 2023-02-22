4139a3d1c0fd074947ae34e6072b67e33fb9ca82
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-02-22 16:23:13 -0300

    feat: initialize new components

---
+++ src/components/Chat/index.tsx
@@ -0,0 +1,7 @@
+interface IChatProps {}
+
+const Chat: React.FC<IChatProps> = (props) => {
+  return <div>CHAT</div>;
+};
+
+export default Chat;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import Auth from "@/components/Auth";
+import Chat from "@/components/Chat";
+import { Box } from "@chakra-ui/react";
+import { NextPageContext } from "next";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const { data } = useSession();
 
-  return (
-    <h1>
-      {data?.user ? (
-        <button onClick={() => signOut()}>Log out</button>
-      ) : (
-        <button onClick={() => signIn("google")}>Sign in</button>
-      )}
-    </h1>
-  );
+  return <Box>{data?.user ? <Chat /> : <Auth />}</Box>;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
